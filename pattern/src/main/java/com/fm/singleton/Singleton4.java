5674e4355ae43a1e7ea16e13b89c6a3f0dfa67c5
jinbaoo <18301667494@163.com>
2017-05-18 14:36:17 +0800

    单例设计模式

--- pattern/src/main/java/com/fm/singleton/Singleton4.java
+++ pattern/src/main/java/com/fm/singleton/Singleton4.java
@@ -1,8 +1,22 @@
 package com.fm.singleton;
 
-/**
+/**双锁 饿汉单例模式
  * @author moore
  * @Date ${date} ${time}
  */
 public class Singleton4 {
+    private static volatile Singleton4 instance = null;
+
+    private Singleton4() {
+    }
+    public static Singleton4 getInstance() {
+        if(null == instance) {
+            synchronized (Singleton4.class) {
+                if(null == instance) {
+                    instance = new Singleton4();
+                }
+            }
+        }
+        return instance;
+    }
 }
